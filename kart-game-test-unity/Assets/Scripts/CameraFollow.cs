9f3d4ee71ee6958b4cb8708cc9bbbe2a8a48b680
CoolBeans19 <alexisuracine@gmail.com>
2018-07-17 18:48:50 -0700

    Tweak camera system

--- kart-game-test-unity/Assets/Scripts/CameraFollow.cs
+++ kart-game-test-unity/Assets/Scripts/CameraFollow.cs
@@ -5,9 +5,15 @@ public class CameraFollow : MonoBehaviour {
 	// Class variables
 	public Transform target; // target transform that the camera follows
 	public Vector3 offset; // vector that determines where the camera will be relative to the target
-	public float speed;
+
+	void Start() {
+		
+	}
 
 	void LateUpdate () {
-		transform.position = target.position + offset;
+		
+		transform.position = target.TransformPoint(offset); // compute position
+
+		transform.LookAt (target); // compute rotation
 	}
 }
---
+++ kart-game-test-unity/Assets/Scripts/CameraStabilize.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraStabilize : MonoBehaviour {
+
+	public Transform target; // target that the camera is following
+
+	void Update () {
+		transform.eulerAngles = new Vector3 (0f, target.eulerAngles.y, 0f); // only follow the y rotation of the car
+	}
+}
